5d7fc6f8d94e073e9e525a22051078df8c93d3de
JaySheen <jyplus01@gmail.com>
2021-12-30 16:50:46 +0900

    member CRUD

--- src/main/java/dev/mvc/member/MemberCont.java
+++ src/main/java/dev/mvc/member/MemberCont.java
@@ -1,5 +1,7 @@
 package dev.mvc.member;
 
+import java.util.List;
+
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -114,5 +116,116 @@ public class MemberCont {
       
       return mav; // forward
     }
+    
+    /**
+     * 목록 출력
+     * @param session
+     * @return
+     */
+     @RequestMapping(value="/member/list.do", method=RequestMethod.GET)
+     public ModelAndView list() {
+       ModelAndView mav = new ModelAndView();
+       
+       List<MemberVO> list = memberProc.list();
+       mav.addObject("list", list);
+
+       mav.setViewName("/member/list"); // /webapp/WEB-INF/views/member/list.jsp
+       
+       return mav;
+     }
+     
+     /**
+      * 회원 조회
+      * @param memberno
+      * @return
+      */
+     @RequestMapping(value="/member/read.do", method=RequestMethod.GET)
+     public ModelAndView read(int memberno){
+       ModelAndView mav = new ModelAndView();
+       
+       MemberVO memberVO = this.memberProc.read(memberno);
+       mav.addObject("memberVO", memberVO);
+       mav.setViewName("/member/read"); // /member/read.jsp
+       
+       return mav; // forward
+     }
+     
+     /**
+      * 회원 정보 수정 처리
+      * @param memberVO
+      * @return
+      */
+     @RequestMapping(value="/member/update.do", method=RequestMethod.POST)
+     public ModelAndView update(MemberVO memberVO){
+       ModelAndView mav = new ModelAndView();
+       
+       // System.out.println("id: " + memberVO.getId());
+       
+       int cnt= memberProc.update(memberVO);
+       
+       if (cnt == 1) {
+         mav.addObject("code", "update_success");
+         mav.addObject("nickname", memberVO.getNickname());  // 회원4님(user4) 회원 정보를 변경했습니다.
+         mav.addObject("id", memberVO.getId());
+       } else {
+         mav.addObject("code", "update_fail");
+       }
+
+       mav.addObject("cnt", cnt); // request.setAttribute("cnt", cnt)
+       mav.addObject("url", "/member/msg");  // /member/msg -> /member/msg.jsp
+       
+       mav.setViewName("redirect:/member/msg.do");
+       
+       return mav;
+     }
+     
+     /**
+      * 회원 삭제
+      * @param memberno
+      * @return
+      */
+     @RequestMapping(value="/member/delete.do", method=RequestMethod.GET)
+     public ModelAndView delete(int memberno){
+       ModelAndView mav = new ModelAndView();
+       
+       MemberVO memberVO = this.memberProc.read(memberno);
+       mav.addObject("memberVO", memberVO);
+       mav.setViewName("/member/delete"); // /member/delete.jsp
+       
+       return mav; // forward
+     }
+    
+     /**
+      * 회원 삭제 처리
+      * @param memberVO
+      * @return
+      */
+     @RequestMapping(value="/member/delete.do", method=RequestMethod.POST)
+     public ModelAndView delete_proc(int memberno){
+       ModelAndView mav = new ModelAndView();
+       
+       // System.out.println("id: " + memberVO.getId());
+       MemberVO memberVO = this.memberProc.read(memberno);
+       
+       
+       int cnt= memberProc.delete(memberno);
+
+       if (cnt == 1) {
+         mav.addObject("code", "delete_success");
+         mav.addObject("nickname", memberVO.getNickname());  // 회원4()님 회원정보삭제에 성공했습니다
+         mav.addObject("id", memberVO.getId());
+       } else {
+         mav.addObject("code", "delete_fail");
+       }
+
+       mav.addObject("cnt", cnt); // request.setAttribute("cnt", cnt)
+       mav.addObject("url", "/member/msg");  // /member/msg -> /member/msg.jsp
+       
+       mav.setViewName("redirect:/member/msg.do");
+       
+       return mav;
+     }
+    
+    
 
 }
